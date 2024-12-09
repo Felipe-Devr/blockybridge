f5f8baed130fdfe3f28e0b58629111ed7e454caa
Felipe-Devr <felipegamerdev1@gmail.com>
2024-12-08 21:25:02 -0700

    feat: added member cache and refactored member

--- caching/member.ts
+++ caching/member.ts
@@ -1,6 +1,60 @@
-import { User } from 'djs/structures';
+import { Guild, GuildMember } from 'djs/structures';
 import { BaseCache } from './base';
+import Client from 'djs/client';
+import { MemberFetchOptions, RawMember, Routes } from 'djs/types';
 
-class MembersCache extends BaseCache<string, User> {}
+class MembersCache extends BaseCache<string, GuildMember> {
+  private guild: Guild;
+
+  public constructor(client: Client, guild: Guild) {
+    super(client);
+    this.guild = guild;
+  }
+
+  /**
+   * Get the cached members of the guild
+   */
+  public getMembers(): Array<GuildMember> {
+    return Array.from(this.cache.values());
+  }
+
+  /**
+   * Fetches the members of the guild and adds them to the cache.
+   * ! Your application need the guild members priviliged intent
+   * ! in order to access the members.
+   */
+  public async fetch(options?: MemberFetchOptions): Promise<Array<GuildMember>> {
+    const response = await this.client.sendAuthenticatedRequest(
+      `${Routes.Guilds}/${this.guild.id}/members`,
+      'Get',
+      options ? JSON.stringify(options) : undefined,
+    );
+
+    if (response.status !== 200) return [];
+    const rawMembers = JSON.parse(response.body) as Array<RawMember>;
+    const members = rawMembers.map((raw) => new GuildMember(this.client, this.guild, raw));
+
+    for (const member of members) {
+      this.cache.set(member.user.id, member);
+    }
+    return members;
+  }
+
+  /**
+   * Resolves the guild member based on the member id.
+   * @param memberId The id of the member to retrieve
+   * @returns The member if existi, otherwise undefined
+   */
+  public async resolve(memberId: string): Promise<GuildMember | undefined> {
+    if (this.cache.has(memberId)) return this.cache.get(memberId);
+    const response = await this.client.sendAuthenticatedRequest(`${Routes.Guilds}/${this.guild.id}/members/${memberId}`, 'Get');
+
+    if (response.status !== 200) return;
+    const member = new GuildMember(this.client, this.guild, JSON.parse(response.body));
+
+    this.cache.set(memberId, member);
+    return member;
+  }
+}
 
 export { MembersCache };
--- structures/member.ts
+++ structures/member.ts
@@ -1,32 +1,62 @@
 import Client from 'djs/client';
 import { User } from './user';
-import { RawMember } from 'djs/types';
+import { BanOptions, RawMember, Routes } from 'djs/types';
+import { Guild } from './guilds';
+import { RoleManager } from 'djs/managers';
 
 class GuildMember {
+  // The internal user of the member
   public readonly user: User;
+
+  // The guild the member is in
+  public readonly guild: Guild;
+
+  // The guild nick the member has
   public nick?: string;
+
+  // The custom per guild avatar if available
   public avatar?: string;
+
+  // The custom per guild banner if available
   public banner?: string;
-  public roles: Array<string> = [];
+
+  // Member Roles
+  public roles: RoleManager;
+
+  // Joined At Timestamp
   public joinedAt: Date;
+
+  // Premium (Booster) since timestamp
   public premiumSince?: Date;
+
+  // Wether or not the user is deafened in voice channels
   public deaf: boolean;
+
+  // Wether or not the user is muted in voice channels
   public mute: boolean;
+
+  // Guild Member flags, represented as a bit set
   public flags: number;
+
+  // Whether the user has not yet passed the guild's Membership Screening requirements
   public pending?: boolean;
+
+  // Total permissions of the member in the channel, including overwrites, returned when in the interaction object
   public permissions: string;
+
   private client: Client;
 
-  public constructor(client: Client, data: RawMember) {
+  public constructor(client: Client, guild: Guild, data: RawMember) {
     this.client = client;
     this.user = new User(client, data.user);
+    this.guild = guild;
 
     const { nick, avatar, banner, roles, joinedAt, premiumSince, deaf, mute, flags, permissions, pending } = data;
 
     this.nick = nick;
     this.avatar = avatar;
     this.banner = banner;
-    this.roles = roles;
+    this.roles = new RoleManager(client, this, roles);
     this.joinedAt = new Date(joinedAt);
     this.premiumSince = premiumSince ? new Date(premiumSince) : undefined;
     this.deaf = deaf;
@@ -34,6 +64,21 @@ class GuildMember {
     this.flags = flags;
     this.pending = pending;
     this.permissions = permissions;
+  }
+
+  public async ban(options?: BanOptions): Promise<void> {
+    await this.client.sendAuthenticatedRequest(
+      `${Routes.Guilds}/${this.guild.id}/bans${this.user.id}/`,
+      'Put',
+      JSON.stringify({
+        reason: options?.reason ?? '',
+        delete_message_seconds: options?.deleteMessagesSeconds ?? 0,
+      }),
+    );
+  }
+
+  public async kick(): Promise<void> {
+    await this.client.sendAuthenticatedRequest(`${Routes.Guilds}/${this.guild.id}/members/${this.user.id}`, 'DELETE');
   }
 }
 
